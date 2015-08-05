f1fce5cc603a01b7a3bf2b2f7ca96cb0857badc4
Javier <jiserte@unq.edu.ar>
2015-08-05 10:26:17 -0300

    CalendarImage Shows a star in the current day.

--- DesktopCalendar/src/org/jiserte/desktopcalendar/CalendarImage.java
+++ DesktopCalendar/src/org/jiserte/desktopcalendar/CalendarImage.java
@@ -7,7 +7,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Polygon;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -27,18 +29,7 @@ public class CalendarImage {
     BufferedImage image = new BufferedImage(dimension.width, dimension.height,
         BufferedImage.TYPE_INT_RGB);
     Graphics2D gi = (Graphics2D) image.getGraphics();
-    gi.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-        RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-    gi.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-        RenderingHints.VALUE_ANTIALIAS_ON);
-    gi.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-        RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-    gi.setRenderingHint(RenderingHints.KEY_RENDERING,
-        RenderingHints.VALUE_RENDER_QUALITY);
-    gi.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-        RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-    gi.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
-        RenderingHints.VALUE_STROKE_NORMALIZE);
+    setRenderingHints(gi);
 
     try {
       BufferedImage in = ImageIO.read(baseImg);
@@ -70,6 +61,15 @@ public class CalendarImage {
     ////////////////////////////////////////////////////////////////////////////
     // Iterate over each column
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Calendar today = Calendar.getInstance();
+    int d = today.get(Calendar.DAY_OF_MONTH);
+    int m = today.get(Calendar.MONTH);
+    int y = today.get(Calendar.YEAR);
+    today.setTimeInMillis(0);
+    today.set(Calendar.DAY_OF_MONTH, d);
+    today.set(Calendar.MONTH, m);
+    today.set(Calendar.YEAR, y);
+    
     int dayCounter = 0;
     for (int col = 0; col < 3; col++) {
       int xDisp =  margins.left + (wdTabWidth + midHorizontalSpacer) * col;
@@ -88,18 +88,7 @@ public class CalendarImage {
                 wdTabHeight, BufferedImage.TYPE_INT_RGB);
             Graphics2D g = (Graphics2D) dayTabImage.getGraphics();
 
-            g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-                RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g.setRenderingHint(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-                RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
-                RenderingHints.VALUE_STROKE_NORMALIZE);
+            setRenderingHints(g);
 
             WorkingDay currentWorkingDay = calendar.getWorkingDay(currentDay);
             Priority currentPriority = currentWorkingDay.getPriority();
@@ -119,6 +108,14 @@ public class CalendarImage {
               g.drawLine(wdTabHeight / 2, wdTabHeight * 3 / 4,
                   wdTabWidth - wdTabHeight / 2, wdTabHeight / 4);
             }
+            if (currentDay.equals(today)) {
+              Polygon starPolygon = this.getStarPolygon(wdTabHeight*0.4,8,wdTabWidth - wdTabHeight / 2,wdTabHeight / 2);
+              g.setColor(new Color(100,210,100));
+              g.fillPolygon(starPolygon);
+              g.setColor(new Color(60, 180, 60));
+              g.setStroke(new BasicStroke(0.5f));
+              g.drawPolygon(starPolygon);
+            }
             AlphaComposite ac = java.awt.AlphaComposite
                 .getInstance(AlphaComposite.SRC_OVER, overlayPercentage);
             gi.setComposite(ac);
@@ -129,8 +126,38 @@ public class CalendarImage {
       }
     }
     ///////////////////////////////////////////////////////////////////////////
+//    gi.setColor(Color.red);
+//    gi.fillPolygon(this.getStarPolygon());
+     return image;
 
-    return image;
+  }
 
+  private Polygon getStarPolygon(double size, int points, int xOffSet, int yOffset) {
+    
+    double angle = Math.PI / points;
+    int[] xpoints = new int[2*points];
+    int[] ypoints = new int[2*points];
+    for (int p = 0; p<points*2;p++) {
+    double alpha = p*angle-Math.PI/2;
+      
+      xpoints[p] = xOffSet +  (int) (Math.cos(alpha)*size*(1-(p%2)*0.5));
+      ypoints[p] = yOffset + (int) (Math.sin(alpha)*size*(1-(p%2)*0.5));
+    }
+    return new Polygon(xpoints, ypoints, points*2);
+  }
+
+  private void setRenderingHints(Graphics2D g) {
+    g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
+        RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+    g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+        RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    g.setRenderingHint(RenderingHints.KEY_RENDERING,
+        RenderingHints.VALUE_RENDER_QUALITY);
+    g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
+        RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+    g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
+        RenderingHints.VALUE_STROKE_NORMALIZE);
   }
 }
